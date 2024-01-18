7c115498c92452bdd3246ee2b947f841bb2d1b26
Roman <Ramenm1001@gmail.com>
2024-01-19 00:38:21 +1000

    commit attt

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Hello world")
-print(1234567)
+x = ()
+print(type(x))
+
+class A:
+    def __init__(self):
+        self.x = 5 # свойства, поля,
+        # аттрибуты, переменные класса
+        # self.x123 = 123
+
+a1 = A()
+setattr(a1, "x123", 123)
+print(a1.x123)
+
+lst = [11, 22, 33, 44, 55]
+#      0   1   2   3   4
+
+print(lst)
+
